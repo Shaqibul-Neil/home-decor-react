ca0dfa5fbcf2abae3926034079b59c83849c5231
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-10-06 03:54:44 +0600

    lottie no search

--- src/Component/NoSearchedProducts.jsx
+++ src/Component/NoSearchedProducts.jsx
@@ -1,5 +1,11 @@
+import Lottie from "lottie-react";
+import emptySearch from "../Lottie/emptySearch.json";
 const NoSearchedProducts = () => {
-  return <div>No Products</div>;
+  return (
+    <div className="flex justify-center py-12">
+      <Lottie animationData={emptySearch} loop={true} className="w-120 h-120" />
+    </div>
+  );
 };
 
 export default NoSearchedProducts;
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -33,16 +33,19 @@ const Products = () => {
           />
         </label>
       </div>
-
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-        {searchedItems.length === 0 ? (
-          <NoSearchedProducts />
-        ) : (
-          searchedItems.map((product) => (
-            <Product key={product.id} product={product} />
-          ))
-        )}
-      </div>
+      {searchedItems.length === 0 ? (
+        <NoSearchedProducts />
+      ) : (
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          {searchedItems.length === 0 ? (
+            <NoSearchedProducts />
+          ) : (
+            searchedItems.map((product) => (
+              <Product key={product.id} product={product} />
+            ))
+          )}
+        </div>
+      )}
     </section>
   );
 };
