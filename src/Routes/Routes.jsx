fe4230327506faf6cb6c114bc1b00491dd49e976
Shaqibul-Neil <shaqibul.islam.neil@gmail.com>
2025-10-06 00:33:41 +0600

    error page

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,11 +3,13 @@ import RootLayout from "../RootLayout/RootLayout";
 import Home from "../Pages/Home";
 import Products from "../Pages/Products";
 import Wishlist from "../Pages/Wishlist";
+import ErrorPage from "../Pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     Component: RootLayout,
+    errorElement: ErrorPage,
     children: [
       { index: true, Component: Home },
       { path: "/products", Component: Products },
